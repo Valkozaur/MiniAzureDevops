48a790b9411b9b142e3300ffe2dc68e149ed9e05
Valentin Nikolov <58659526+Valkozaur@users.noreply.github.com>
2021-06-20 09:43:26 +0300

    Remove Extension method

--- MiniAzureDevops.ItemTable.Application/Features/Table/Queries/GetTableById/TableVm.cs
+++ MiniAzureDevops.ItemTable.Application/Features/Table/Queries/GetTableById/TableVm.cs
@@ -1,15 +1,12 @@
-﻿using MiniAzureDevops.ItemTable.Domain.Entities;
+﻿using MiniAzureDevops.ItemTable.Application.Common.Extensions.Mapping;
 using System;
-using System.Collections.Generic;
 
 namespace MiniAzureDevops.ItemTable.Application.Features.Table.Queries.GetTableById
 {
-    public class TableVm
+    public class TableVm : IMapFrom<Domain.Entities.Table>
     {
         public Guid Id { get; set; }
 
         public string Name { get; set; }
-
-        public HashSet<Column> Columns { get; set; } = new HashSet<Column>();
     }
 }
